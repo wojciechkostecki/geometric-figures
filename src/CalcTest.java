bcd682a97d7b64e9101155d45ccda370d38ca754
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-01 20:06:01 +0200

    Creation of methody pointInCircle and creating a test class

---
+++ src/CalcTest.java
@@ -0,0 +1,17 @@
+public class CalcTest {
+    public static void main(String[] args) {
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(3, 7);
+        Point p3 = new Point(5, 4);
+        Point p4 = new Point(5, 7);
+        Rectangle rectangle = new Rectangle(p1, p2, p3, p4);
+
+        Calculator.rectangleArea(rectangle);
+
+        Point p = new Point(6, 8);
+        Point x = new Point(4, 4);
+        Circle circle = new Circle(p, 3);
+        
+        Calculator.pointInCircle(circle, x);
+    }
+}
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public static void rectangleArea(Rectangle rectangle){
+    public static void rectangleArea(Rectangle rectangle) {
         double x1 = rectangle.getP1().x;
         double y1 = rectangle.getP1().y;
         double x2 = rectangle.getP2().x;
@@ -8,13 +8,21 @@ public class Calculator {
         double x3 = rectangle.getP3().x;
         double y3 = rectangle.getP3().y;
 
-        double a = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+        double a = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
         double sideA = Math.sqrt(a);
 
-        double b = ((x3-x2)*(x3-x2))+((y3-y2)*(y3-y2));
+        double b = ((x3 - x2) * (x3 - x2)) + ((y3 - y2) * (y3 - y2));
         double sideB = Math.sqrt(b);
 
-        System.out.println(sideA*sideB);
+        System.out.println(sideA * sideB);
     }
 
+    public static void pointInCircle(Circle circle, Point x) {
+        double z = ((x.x - circle.s.getX()) * (x.x - circle.s.getX())) + ((x.y - circle.s.getY()) * (x.y - circle.s.getY()));
+        if (z == (circle.r * circle.r)) {
+            System.out.println("Punkt leży wewnątrz okręgu");
+        }else {
+            System.out.println("Punkt NIE leży wewnątrz okręgu");
+        }
+    }
 }
